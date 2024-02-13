45cc3945929d41d4bb2d5066b1274b6afc945d51
kavya <kavyabhalodia22@gmail.com>
2024-02-13 18:05:07 +0530

    change in code

--- variable.tf
+++ variable.tf
@@ -1,15 +1,15 @@
 
-# variable "public_subnet_cidr" {
-#   type        = list(string)
-#   description = "Public subnet cidr"
+variable "public_subnet_cidr" {
+  type        = list(string)
+  description = "Public subnet cidr"
 
-# }
-# variable "private_subnet_cidr" {
-#   type        = list(string)
-#   description = "Private subnet cidr"
+}
+variable "private_subnet_cidr" {
+  type        = list(string)
+  description = "Private subnet cidr"
 
-# }
-# variable "az" {
-#   type        = list(string)
-#   description = "Avalaibility zone for subnets"
-# }
+}
+variable "az" {
+  type        = list(string)
+  description = "Avalaibility zone for subnets"
+}
