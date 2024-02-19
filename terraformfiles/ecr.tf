12012dbc7ccad20f556f67df73d81b7e3be70108
kavya <kavyabhalodia22@gmail.com>
2024-02-19 17:41:50 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_ecr_repository" "images" {
   name                 = "images"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
