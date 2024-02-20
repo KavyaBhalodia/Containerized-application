5b88d428189ca490a4ac6bb6a7663a7c9604f82d
kavya <kavyabhalodia22@gmail.com>
2024-02-20 11:39:07 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -15,15 +15,15 @@ resource "null_resource" "docker_packaging" {
 provisioner "local-exec" {
   command = <<EOT
     $GIT_COMMIT_ID = git rev-parse --short HEAD
-    docker build -t images:$GIT_COMMIT_ID  .
+    docker build -t kavya:$GIT_COMMIT_ID  .
     
-    docker tag images:$GIT_COMMIT_ID 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:$GIT_COMMIT_ID
-    docker tag images:latest 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:latest
+    docker tag kavya:$GIT_COMMIT_ID 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:$GIT_COMMIT_ID
+   
 
     aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
 	  
     docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"
-    docker push "${aws_ecr_repository.images.repository_url}:latest"
+    
 
   EOT
 
