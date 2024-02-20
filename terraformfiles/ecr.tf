5251e9289c3bf6541279ff13a4e1d939ba11730f
kavya <kavyabhalodia22@gmail.com>
2024-02-20 14:14:15 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -17,7 +17,7 @@ provisioner "local-exec" {
     $GIT_COMMIT_ID = git rev-parse --short HEAD
     aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
     docker build -t containerized-application .
-    docker tag containerized-application:$GIT_COMMIT_ID 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:$GIT_COMMIT_ID
+    docker tag containerized-application:latest 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:$GIT_COMMIT_ID
     docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"    
 
   EOT
