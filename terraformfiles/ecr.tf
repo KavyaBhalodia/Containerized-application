1cbfe661b765521a9b6b3dbfa6f870a674dc291a
kavya <kavyabhalodia22@gmail.com>
2024-02-20 15:33:05 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -15,14 +15,13 @@ resource "null_resource" "docker_packaging" {
 provisioner "local-exec" {
   command = <<EOT
     $GIT_COMMIT_ID = git rev-parse --short HEAD
-    
     docker build -t containerized-application .
     aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com 
     docker tag containerized-application "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"
     docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"   
 
   EOT
-  interpreter = ["PowerShell", "-Command"]
+ interpreter = ["PowerShell", "-Command"]
 }
 	  triggers = {
 	    "run_at" = timestamp()
