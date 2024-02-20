ae79aea9a7afbffe505273ee20b751cbd2506b78
kavya <kavyabhalodia22@gmail.com>
2024-02-20 11:17:56 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -10,16 +10,16 @@ resource "aws_ecr_repository" "images" {
     scan_on_push = true
   }
 }
-//docker build -t "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID" .
+//aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
 resource "null_resource" "docker_packaging" {
 provisioner "local-exec" {
   command = <<EOT
     $GIT_COMMIT_ID = git rev-parse --short HEAD
-    aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
-    docker build -t images .
-    docker tag images:$GIT_COMMIT_ID ${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID
-	  docker push ${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID
-      
+    docker build -t "${aws_ecr_repository.images.repository_url}:latest" .
+    
+    aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
+	  docker push "${aws_ecr_repository.images.repository_url}:latest"
+    docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"
   EOT
 
   interpreter = ["PowerShell", "-Command"]
--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -1,6 +1,11 @@
 provider "aws" {
   region = "ap-south-1"
 }
+provider "aws" {
+  alias = "sandbox"
+  region = "us-west-2"
+  
+}
 
 terraform {
   required_providers {
