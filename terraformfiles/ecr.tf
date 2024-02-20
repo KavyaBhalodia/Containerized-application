d74c6c7d2c573695ddbb5094a570c45e6b41340f
kavya <kavyabhalodia22@gmail.com>
2024-02-20 14:54:11 +0530

    change in code

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -15,11 +15,14 @@ resource "null_resource" "docker_packaging" {
 provisioner "local-exec" {
   command = <<EOT
     $GIT_COMMIT_ID = git rev-parse --short HEAD
-    aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
+    
     docker build -t containerized-application .
-    docker push "${aws_ecr_repository.images.repository_url}" 
-    docker tag containerized-application:latest 831794387446.dkr.ecr.ap-south-1.amazonaws.com/images:$GIT_COMMIT_ID
-    docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"    
+    aws ecr get-login-password --region ap-south-1 |  docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-south-1 ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.ap-south-1.amazonaws.com
+    docker tag containerized-application "${aws_ecr_repository.images.repository_url}:latest" 
+    docker tag containerized-application "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"
+    
+    docker push "${aws_ecr_repository.images.repository_url}:$GIT_COMMIT_ID"
+    docker push "${aws_ecr_repository.images.repository_url}:latest"     
 
   EOT
 
