84635472e3d27d38587ac7d7bdc9f9c22b3717fe
kavya <kavyabhalodia22@gmail.com>
2024-03-04 18:26:17 +0530

    change in code

--- terraformfiles/databse.tf
+++ terraformfiles/databse.tf
@@ -1,29 +1,29 @@
-# #RDS postgres database for form
-# resource "aws_db_instance" "default" {
-#   provider               = aws.sandbox
-#   allocated_storage      = 20
-#   db_name                = "postgres"
-#   identifier             = "kavya-database"
-#   engine                 = "postgres"
-#   engine_version         = "16.1"
-#   instance_class         = "db.t3.micro"
-#   username               = "postgres"
-#   password               = "kavyabhalodia"
-#   skip_final_snapshot    = true
-#   storage_type           = "gp2"
-#   db_subnet_group_name   = aws_db_subnet_group.subnet-grp.name
-#   vpc_security_group_ids = [aws_security_group.rds-sg.id]
-#   availability_zone      = "us-west-2a"
-# }
+#RDS postgres database for form
+resource "aws_db_instance" "default" {
+  provider               = aws.sandbox
+  allocated_storage      = 20
+  db_name                = "postgres"
+  identifier             = "kavya-database"
+  engine                 = "postgres"
+  engine_version         = "16.1"
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  password               = "kavyabhalodia"
+  skip_final_snapshot    = true
+  storage_type           = "gp2"
+  db_subnet_group_name   = aws_db_subnet_group.subnet-grp.name
+  vpc_security_group_ids = [aws_security_group.rds-sg.id]
+  availability_zone      = "us-west-2a"
+}
 
-# #subnet-grp for database
-# resource "aws_db_subnet_group" "subnet-grp" {
-#   name       = "main-grp"
-#   subnet_ids = aws_subnet.private-subnet.*.id
-#   provider   = aws.sandbox
-# }
+#subnet-grp for database
+resource "aws_db_subnet_group" "subnet-grp" {
+  name       = "main-grp"
+  subnet_ids = aws_subnet.private-subnet.*.id
+  provider   = aws.sandbox
+}
 
-# #dynamo-db table for state-locking
+#dynamo-db table for state-locking
 # resource "aws_dynamodb_table" "conainerized-application-dynamodb-table" {
 #   provider       = aws.sandbox
 #   name           = "containerized-application-tf-lockID"
