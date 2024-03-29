9c071544e639b9e4129cf04687f2a347ec639d9c
KavyaBhalodia <kavyabhalodia22@gmail.com>
2024-03-29 14:53:38 +0530

    code

--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -19,6 +19,8 @@ terraform {
     key     = "state/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
+    access_key = "AKIA4DKWPDH3GXUAHPMR"
+    secret_key = "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"
     
     # dynamodb_table = "containerized-application-tf-lockID"
   }
