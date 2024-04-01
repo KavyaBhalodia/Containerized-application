a7a45c236cdfc094bf893fb4ca729a2dbb9eb153
KavyaBhalodia <kavyabhalodia22@gmail.com>
2024-04-01 17:21:45 +0530

    Changes in jenkinsfile

--- terraformfiles/bastion_host.tf
+++ terraformfiles/bastion_host.tf
@@ -1,10 +1,10 @@
-#module for bastion_host
-module "bastion_host_name" {
-  source            = "./modules/bastion_host.tf"
-  bastion_host_name = "${local.env}-containered_app_bastion_host"
-  subnet_id         = module.vpc.public_subnet_id
-  bastion_host_sg   = [module.bastion_host_security_group.sg_id]
-  providers = {
-    aws = aws
-  }
-}
+# #module for bastion_host
+# module "bastion_host_name" {
+#   source            = "./modules/bastion_host.tf"
+#   bastion_host_name = "${local.env}-containered_app_bastion_host"
+#   subnet_id         = module.vpc.public_subnet_id
+#   bastion_host_sg   = [module.bastion_host_security_group.sg_id]
+#   providers = {
+#     aws = aws
+#   }
+# }
