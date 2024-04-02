ba25009b085c734c8da0f240b76b86b198172a86
KavyaBhalodia <kavyabhalodia22@gmail.com>
2024-04-02 14:43:47 +0530

    script path change

--- terraformfiles/git-branch.tf
+++ terraformfiles/git-branch.tf
@@ -1,12 +1,10 @@
-#external block to get current git-branch
+# external block to get current git-branch
 data "external" "git_branch" {
-  program = ["Powershell", "powershell.exe -ExecutionPolicy Bypass -File ${path.module}\\script.ps1"]
+  program = ["Powershell", "powershell.exe -ExecutionPolicy Bypass -File ..\\code\\script.ps1"]
 }
 
 #local block store current git-branch
 locals {
   env = data.external.git_branch.result["branch"]
 }
-
-
 
--- terraformfiles/script.ps1
+++
@@ -1,6 +0,0 @@
-$GIT_BRANCH=git rev-parse --abbrev-ref HEAD
-Write-Output "{ 
-`"branch`": `"$GIT_BRANCH`" 
-}"
-
-
