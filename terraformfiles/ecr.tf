b6e4ef14a63b0554337cb7b46387aa93a1bdc502
KavyaBhalodia <kavyabhalodia22@gmail.com>
2024-04-02 11:16:17 +0530

    script path change

--- terraformfiles/ecr.tf
+++ terraformfiles/ecr.tf
@@ -1,10 +1,10 @@
-#module for ECR
-module "ecr" {
-  source         = "./modules/ecr"
-  ecr_repository = "${local.env}-containerized_app_repository"
-  region         = "ap-south-1"
-  providers = {
-    aws = aws
-  }
-}
+# #module for ECR
+# module "ecr" {
+#   source         = "./modules/ecr"
+#   ecr_repository = "${local.env}-containerized_app_repository"
+#   region         = "ap-south-1"
+#   providers = {
+#     aws = aws
+#   }
+# }
 
